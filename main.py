99d272338250c5a5c460a667a21cfcce01205fef
RadKlo <rad.klos40@gmail.com>
2025-02-14 14:28:46 +0100

    Initial Commit

---
+++ main.py
@@ -0,0 +1,13 @@
+a = 15
+b = 43.5
+for i in range(30):
+    print("Hello world")
+
+class test:
+    tekst = "bla"
+    czy = True
+    def oblicz(self, a, b):
+        c = a + b
+        print(c)
+
+
